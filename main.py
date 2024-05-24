f744f64a6626d27a3b411066d54a8d854a5b5b34
Gabriel Campillo <gcampillo305@icloud.com>
2024-05-23 20:22:52 -0400

    Updated to POST

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import (
+    Flask,
+    request
+)
 
 app = Flask(__name__)
 
 @app.route("/tradingview/webhook", methods=["POST"])
 def tradingview_hook():
+    data = request.get_json()
+    print(data)
     return {}, 200
 
 
