7f112faf4d1acf7a937bbbd6a4a408984ffc088c
Gabriel Campillo <gcampillo305@icloud.com>
2024-05-23 20:07:09 -0400

    Set webhook to be a post

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/tradingview/webhook")
+@app.route("/tradingview/webhook", methods=["POST"])
 def tradingview_hook():
     return {}, 200
 
