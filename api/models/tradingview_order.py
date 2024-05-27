c43a4d0f0af7e4782abeb8dcf20e82dcb1cd87e4
Gabriel Campillo <gcampillo305@icloud.com>
2024-05-27 16:21:32 -0400

    Added addtional fields

--- api/models/tradingview_order.py
+++ api/models/tradingview_order.py
@@ -15,8 +15,10 @@ class TradingViewOrder:
     time: str
     equity: Equity
     buy_sell_amount: float
+    price: float
+    market_position: str
 
-    def __init__(self, ticker: str, action: str, contracts: float, position_size: float, interval: int, close: float, time: str, alpaca_price_ticker: str, equity: str, buy_sell_amount: float):
+    def __init__(self, ticker: str, action: str, contracts: float, position_size: float, interval: int, close: float, time: str, alpaca_price_ticker: str, equity: str, buy_sell_amount: float, price: float, market_position: str):
         self.ticker = ticker
         self.alpaca_price_ticker = alpaca_price_ticker
         self.action = TradingViewAction.from_str(action)
@@ -27,6 +29,8 @@ class TradingViewOrder:
         self.time = time
         self.equity = Equity.from_str(equity)
         self.buy_sell_amount = buy_sell_amount
+        self.price = price
+        self.market_position = market_position
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} ticker={self.ticker} action={self.action} contracts={self.contracts} position_size={self.position_size} interval={self.interval} close={self.close} time={self.time}>"
