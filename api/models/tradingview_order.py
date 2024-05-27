71be0fd7f83b97b9c585bcbfc958f1e0f49c1fec
Gabriel Campillo <gcampillo305@icloud.com>
2024-05-27 16:23:13 -0400

    updated repr

--- api/models/tradingview_order.py
+++ api/models/tradingview_order.py
@@ -33,4 +33,4 @@ class TradingViewOrder:
         self.market_position = market_position
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} ticker={self.ticker} action={self.action} contracts={self.contracts} position_size={self.position_size} interval={self.interval} close={self.close} time={self.time}>"
+        return f"<{self.__class__.__name__} ticker={self.ticker} action={self.action} contracts={self.contracts} position_size={self.position_size} interval={self.interval} close={self.close} time={self.time} price={self.price} market_position={self.market_position}>"
